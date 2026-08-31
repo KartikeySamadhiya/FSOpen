7ac6d3d47afb99db198481b6356f8a56fa44fa32
KartikeySamadhiya <kartikeys.ug23.cs@nitp.ac.in>
2026-08-31 15:24:23 +0530

    part3 done

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -12,25 +12,25 @@ app.use(cors())
 app.use(express.static('dist'))
 
 let persons = [
-  { 
-    id: "1",
-    name: "Arto Hellas", 
-    number: "040-123456"
+  {
+    id: '1',
+    name: 'Arto Hellas',
+    number: '040-123456'
   },
-  { 
-    id: "2",
-    name: "Ada Lovelace", 
-    number: "39-44-5323523"
+  {
+    id: '2',
+    name: 'Ada Lovelace',
+    number: '39-44-5323523'
   },
-  { 
-    id: "3",
-    name: "Dan Abramov", 
-    number: "12-43-234345"
+  {
+    id: '3',
+    name: 'Dan Abramov',
+    number: '12-43-234345'
   },
-  { 
-    id: "4",
-    name: "Mary Poppendieck", 
-    number: "39-23-6423122"
+  {
+    id: '4',
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122'
   }
 ]
 
@@ -53,14 +53,14 @@ app.get('/info', (request, response) => {
 
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(p => p.id === id)
+  const id = request.params.id
+  const person = persons.find(p => p.id === id)
 
-    if(person){
-        response.json(person)
-    }else{
-        response.status(404).end()
-    }
+  if(person){
+    response.json(person)
+  }else{
+    response.status(404).end()
+  }
 })
 
 
@@ -69,35 +69,35 @@ app.put('/api/persons/:id', (request, response, next) => {
 
   Person.findByIdAndUpdate(   //findByIdAndUpdate returns the document as it was before the update took place.
     request.params.id,
-    {name , number},
+    { name , number },
     { new: true, runValidators: true, context: 'query' }    //new: true forces Mongoose to return the newly updated document instead
   )     //By default, Mongoose schema validation rules (such as required: true or minlength: 8) automatically run when creating new documents with .save(), but are ignored during updates. Setting runValidators: true forces Mongoose to validate the incoming data against your personSchema rules
-  .then(updatedPerson => {
-    if(updatedPerson){
+    .then(updatedPerson => {
+      if(updatedPerson){
         response.json(updatedPerson)
-    }else{
+      }else{
         response.status(400).end()
-    }
-  })
-  .catch(error => next(error))
-})
-
-
-app.delete('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-    .then(result =>{
-        response.status(204).end()
+      }
     })
     .catch(error => next(error))
 })
 
 
-app.post('/api/persons', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
   /*if (!body.name || !body.number) {
-    return response.status(400).json({ 
-      error: 'name or number missing' 
+    return response.status(400).json({
+      error: 'name or number missing'
     })
   }*/
 
@@ -107,10 +107,10 @@ app.post('/api/persons', (request, response) => {
   })
 
   person.save()
-  .then(savedPerson => {    //earlier we put checks here like body should have name, number
-    response.json(savedPerson)//now we moved over checks in person.js(mongo)
-  })                    //so we add person her, it goes there if invalid error 
-  .catch(error => next(error)) //and this handles that error
+    .then(savedPerson => {    //earlier we put checks here like body should have name, number
+      response.json(savedPerson)//now we moved over checks in person.js(mongo)
+    })                    //so we add person her, it goes there if invalid error
+    .catch(error => next(error)) //and this handles that error
 })
 
 
@@ -127,7 +127,7 @@ const errorhandler = (error, request, response, next) => {
 }             //pass to Express Built-in Error Handler
 
 
-app.use(errorhandler) //this has to be the last loaded middleware, otherwise it will not work 
+app.use(errorhandler) //this has to be the last loaded middleware, otherwise it will not work
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -10,7 +10,7 @@ if (process.argv.length < 3) {
 
 const password = process.argv[2]
 
-const url = `mongodb+srv://phonebook:testpasspbook@cluster0.uzgkpvv.mongodb.net/?appName=Cluster0`
+const url = process.env.MONGODB_URI
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
@@ -24,32 +24,32 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 
 if(process.argv.length === 3){
-    Person.find({}).then(result => {
-        console.log('phonebook:')
-        result.forEach(person => {
-            console.log(`${person.name} ${person.number}`)
-        })
-        mongoose.connection.close()
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
     })
+    mongoose.connection.close()
+  })
 }
 
 // If password, name, and number arguments are passed, create a new entry
 else if (process.argv.length === 5){
-    const name = process.argv[3]
-    const number = process.argv[4]
+  const name = process.argv[3]
+  const number = process.argv[4]
 
-    const person = new Person({
-        name: name,
-        number: number,
-    })
+  const person = new Person({
+    name: name,
+    number: number,
+  })
 
-    person.save().then(() =>{
-        console.log(`added ${name} number ${number} to phonebook`)
-        mongoose.connection.close()
-    })
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
 
 else{
-    console.log('Usage: node mongo.js <password> [<name>] [<number>]')
-    mongoose.connection.close()
+  console.log('Usage: node mongo.js <password> [<name>] [<number>]')
+  mongoose.connection.close()
 }
